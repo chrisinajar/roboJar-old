594b1f094a9a92619d8bd6361c6e7a98eb86eaa1
Chris Vickery <chrisinajar@gmail.com>
2012-02-18 05:03:27 -0500

    It's not a real project without license

--- robotJar/run.js
+++ robotJar/run.js
@@ -1,3 +1,21 @@
+/*
+ * roboJar: A turntable.fm bot. Chris "inajar" Vickery <chrisinajar@gmail.com>
+ *
+ * Redistribution and use in source, minified, binary, or any other forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name, roboJar, nor the names of its contributors may be 
+ *    used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * Needs more cat facts.
+ *
+ */
+
 // load my shit!
 var Bot = require('../ttapi/index'),
     readline = require('readline'),
