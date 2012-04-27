804f9b5c59566689afa96bcae224f33cc2ac8b60
Chris Vickery <chrisinajar@gmail.com>
2012-04-26 20:11:07 -0400

    Added example config file

---
+++ robotJar/config.example.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+	// The bot's name
+	name: 'AwesomeBot',
+	
+	// The auth token used to authenticated with ttfm. It's saved in your cookies.
+	auth: 'auth+live+1234567890abcdef1234567890abcdef12345678',
+	
+	// Userid to log in with
+	userid: '1234567890abcdef12345678',
+	
+	// Roomid to autojoin
+	roomid: '1234567890abcdef12345678',
+	
+	// Array of modules to load automatically
+	autoload: [
+		'admin',
+		'pm'
+	],
+	
+	// Something used by modules a lot...
+	specialUsers: {
+		'chrisinajar': '4e42c21b4fe7d02e6107b1ff'
+	}
+};
